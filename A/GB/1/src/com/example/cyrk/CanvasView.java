a7e6ca11dac51fc8c4e3e6fe4519c6bfbfce66f2
pal73_book <pal@ngs.ru>
2015-11-08 23:18:21 +0600

    http://geekbrains.ru/records/1060

---
+++ A/GB/1/src/com/example/cyrk/CanvasView.java
@@ -0,0 +1,37 @@
+package com.example.cyrk;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 07.11.2015.
+ */
+public class CanvasView extends View{
+    private MainCircle mainCircle;
+    private Paint paint;
+
+    public CanvasView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initMainCircle();
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    private void initMainCircle() {
+        mainCircle = new MainCircle(200,500);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(mainCircle.getX(),mainCircle.getY(),mainCircle.getRadius(),paint);
+    }
+}
---
+++ A/GB/1/src/com/example/cyrk/MainCircle.java
@@ -0,0 +1,32 @@
+package com.example.cyrk;
+
+/**
+ * Created by user on 07.11.2015.
+ */
+public class MainCircle {
+    public static final int INT = 50;
+    private int x;
+    private int y;
+    private int radius;
+
+    public MainCircle(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.radius = INT;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+
+
+}
