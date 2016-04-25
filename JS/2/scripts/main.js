0d7c20b3af16056cb3d458a7d22a5e64784bbe8f
PAL_WW <pal@ngs.ru>
2016-04-25 16:37:23 +0600

    123

--- JS/2/scripts/main.js
+++ JS/2/scripts/main.js
@@ -2,10 +2,11 @@ var canvas = document.querySelector("#canvas");
 var context = canvas.getContext("2d");
 var canvas_box = document.querySelector("#canvas_box");
 
-canvas_box.setAttribute("width","700");
-canvas_box.margin="auto";
-canvas.setAttribute("width","500");
-canvas.setAttribute("height","400");
+//canvas_box.setAttribute("width","700");
+//canvas_box.margin="auto";
+//canvas_box.setAttribute("style", "width:550px");
+//r canvas.setAttribute("style","width:500px")
+//canvas.setAttribute("height","400");
 //canvas_box.setAttribute("margin","auto");
-context.fillRect(30,30,10,10);
+//context.fillRect(30,30,10,10);
 
