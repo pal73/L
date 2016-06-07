d19ba300da65fc239299fb893d6c52c6f027943f
PAL_WW <pal@ngs.ru>
2016-06-07 17:24:37 +0600

    FAPP

--- W/FAPP/fapp.js
+++ W/FAPP/fapp.js
@@ -4,4 +4,12 @@
 window.onload=function(){
   var new_div = document.createElement("div");
   new_div.textContent="345";
+  document.body.appendChild(new_div);
+  illumine(300);
 };
+
+function illumine(timeInterval){
+  var temp = parseInt(document.getElementsByName("div")[1].innerHTML);
+  document.getElementsByName("div")[1].textContent=++temp;
+  setTimeout(illumine(300),timeInterval);
+}
