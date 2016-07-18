4ff6475f7bc7aa89c280a3bb2600d63d7ff658f0
PAL73 <palnsk73@gmail.com>
2016-07-18 16:54:18 +0600

    123

--- JS/Crafty/1/src/game.js
+++ JS/Crafty/1/src/game.js
@@ -2,10 +2,22 @@
  * Created by PAL on 14.07.2016.
  */
 var myGame = {
-    start: function(){
-        Crafty.init(400,300);
-        Crafty.background('rgb(249, 3, 125)');
+    timeCnt: 0,
+    timeCntMax: 20,
 
+    start: function(){
+        Crafty.init(400,300,"game_body");
+        Crafty.background('rgb(249, 3, 125)');
+        var myText = Crafty.e('2D, DOM, Color, Text')
+            .attr({
+                x: 20,
+                y: 20
+            })
+            .textColor('green')
+            .textFont({
+                size: '20px'
+            });
+        myText.text("Hello!!!");
         var mife = Crafty.e("2D,Canvas,Color")
             .attr({
                 x:20,
@@ -16,8 +28,28 @@ var myGame = {
             .color('#333')
             .bind("EnterFrame", function(eventData) {
                 // Move to the right by 10 pixels per second
-                this.x = this.x + 10 * (eventData.dt / 1000);
+                //this.x = this.x + 10 * (eventData.dt / 1000);
+                myGame.timeCnt += eventData.dt;
+                if (myGame.timeCnt >= myGame.timeCntMax) {
+                    myGame.timeCnt = 0;
+                    this.x += 1;
+                    var now = new Date();
+                    myText.text(now.getSeconds());
+                //this.x = this.x + 10;
+                }
             });
+        var textBox=Crafty.e("2D,Canvas,Color,Text")
+            .attr({
+                x:100,
+                y:100,
+                w:40,
+                h:40
+            })
+            //.textFont({
+               // size:'20px'
+            //})
+            //.text('10000')
+            .color('#ffffff');
         console.log("STOPSTARTING");
     }
 }
