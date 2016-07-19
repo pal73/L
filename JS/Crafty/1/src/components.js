54dac2f8af14eef84725a96451b6149804392694
pal73_book <pal@ngs.ru>
2016-07-19 22:54:02 +0600

    123

--- JS/Crafty/1/src/components.js
+++ JS/Crafty/1/src/components.js
@@ -1,3 +1,19 @@
 /**
  * Created by PAL on 14.07.2016.
  */
+Crafty.c('Mytextbox',{
+    init: function(){
+        this.requires("2D,Canvas,Color,Text")
+            .attr({
+                x:200,
+                y:200,
+                w:40,
+                h:40
+            })
+            .textFont({
+                size:'20px'
+            })
+            //.text('10000')
+            .color('#ffffff');
+    },
+});
