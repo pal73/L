e7f67bd4af3e440e8787f186805abe94e92e8244
PAL73 <palnsk73@gmail.com>
2016-11-02 17:51:00 +0700

    PJS4

---
+++ JS/PJS/PJS4/init.js
@@ -0,0 +1,21 @@
+var pjs = new PointJS('2d', 600, 600);
+//pjs.system.initFullPage();
+
+var vector = pjs.vector;
+var log = pjs.system.log;
+var game = pjs.game;
+var point = vector.point;
+var size = vector.size;
+var camera = pjs.camera;
+var brush = pjs.brush;
+var OOP = pjs.OOP;
+var math = pjs.math;
+
+var key = pjs.keyControl;
+key.initKeyControl();
+
+//var mouse = pjs.mouseControl;
+//mouse.initMouseControl();
+
+//var touch = pjs.touchControl;
+//touch.initTouchControl();
